0a30807b9143357dbd5241d809c98c70164e1c10
Sylvestre <pasytchangwa@gmail.com>
2021-08-17 19:54:40 +0300

    Add comment to ignore linters requirement

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file, react/prefer-stateless-function */
 import React from 'react';
 import CalcApp from './components/Calculator';
 import './components/style.css';
