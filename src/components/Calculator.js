38133e8df2a2edff184cfb5aa0295b728a807a12
Sylvestre <pasytchangwa@gmail.com>
2021-08-18 16:30:57 +0300

    Implement the event handlers using the logic from calculator.js and operator.js files

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,77 +1,161 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import './style.css';
 import calculate from '../logic/calculator';
 
-export default class CalcApp extends React.Component {
+export default class CalcApp extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      res: 0,
-    };
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {};
   }
 
+  handleClick = (buttonName) => {
+    this.setState((state) => calculate(state, buttonName));
+  };
+
+  showSolution = () => {
+    const { total, next, operation } = this.state;
+    const solution = (total || '') + (next || '') + (operation || '');
+
+    return (solution || '');
+  };
+
   render() {
-    const { res } = this.state;
     return (
       <div className="wrapper">
-        <span className="operation-result">{res}</span>
+        <span className="operation-result">{this.showSolution()}</span>
         <section className="main-section">
-          <button type="button" className="left-most cell1">
+          <button
+            type="button"
+            className="left-most cell1"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             AC
           </button>
-          <button type="button" className="left-most cell2">
+          <button
+            type="button"
+            className="left-most cell2"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             +/-
           </button>
-          <button type="button" className="left-most cell3">
+          <button
+            type="button"
+            className="left-most cell3"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             %
           </button>
-          <button type="button" className="right-most cell4">
+          <button
+            type="button"
+            className="right-most cell4"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             /
           </button>
-          <button type="button" className="left-most cell5">
+          <button
+            type="button"
+            className="left-most cell5"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             7
           </button>
-          <button type="button" className="left-most cell6">
+          <button
+            type="button"
+            className="left-most cell6"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             8
           </button>
-          <button type="button" className="left-most cell7">
+          <button
+            type="button"
+            className="left-most cell7"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             9
           </button>
-          <button type="button" className="right-most cell8">
+          <button
+            type="button"
+            className="right-most cell8"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             x
           </button>
-          <button type="button" className="left-most cell9">
+          <button
+            type="button"
+            className="left-most cell9"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             4
           </button>
-          <button type="button" className="left-most cell10">
+          <button
+            type="button"
+            className="left-most cell10"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             5
           </button>
-          <button type="button" className="left-most cell11">
+          <button
+            type="button"
+            className="left-most cell11"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             6
           </button>
-          <button type="button" className="right-most cell12">
+          <button
+            type="button"
+            className="right-most cell12"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             -
           </button>
-          <button type="button" className="left-most cell13">
+          <button
+            type="button"
+            className="left-most cell13"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             1
           </button>
-          <button type="button" className="left-most cell14">
+          <button
+            type="button"
+            className="left-most cell14"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             2
           </button>
-          <button type="button" className="left-most cell15">
+          <button
+            type="button"
+            className="left-most cell15"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             3
           </button>
-          <button type="button" className="right-most cell16">
+          <button
+            type="button"
+            className="right-most cell16"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             +
           </button>
-          <button type="button" className="left-most cell17">
+          <button
+            type="button"
+            className="left-most cell17"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             0
           </button>
-          <button type="button" className="left-most cell18">
+          <button
+            type="button"
+            className="left-most cell18"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             .
           </button>
-          <button type="button" className="right-most cell19">
+          <button
+            type="button"
+            className="right-most cell19"
+            onClick={(ev) => this.handleClick(ev.target.name)}
+          >
             =
           </button>
         </section>
