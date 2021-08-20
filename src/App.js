7932487f1004a804ed6c23c2e07c5f3bd22ed635
Sylvestre <pasytchangwa@gmail.com>
2021-08-20 12:23:43 +0300

    Fix linters errors in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,6 @@ import React from 'react';
 import CalcApp from './components/Calculator';
 import './components/style.css';
 
-const App = () => {return <CalcApp />;};
+const App = () => { return <CalcApp />; };
 
 export default App;
