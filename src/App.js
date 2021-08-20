994c2d1b0c391565563ee96467d5a6b95ee39305
Sylvestre <pasytchangwa@gmail.com>
2021-08-20 12:16:34 +0300

    Fix newline issue in app.js for linters

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from 'react';
 import CalcApp from './components/Calculator';
 import './components/style.css';
 
-const App = () => {
-  return <CalcApp />;
-}
+const App = () => { return <CalcApp />;
+};
 
 export default App;
