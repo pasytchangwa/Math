9e1e5dfdbf1451abcba390343bd722fbfa9ccd54
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 18:01:09 +0300

    Update nav,js file

--- src/Nav.js
+++ src/Nav.js
@@ -1,3 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
+
+const Nav = () => {
+  const navStyle = {
+   color: 'white',
+  };
+  return (
+    <div>
+      <nav>
+        <h2 className="title">Math magicians</h2>
+        <ul>
+          <Link to="/">
+            <li style={navStyle}>Home</li>
+          </Link>
+          <Link to="/calculator">
+            <li className="middle" style={navStyle}>
+              Calculator
+            </li>
+          </Link>
+          <Link to="/quote">
+            <li style={navStyle}>Quote</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
