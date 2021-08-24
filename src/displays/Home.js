0a05d1e1cbeb31d4afc27f79212c07568d4f0b8f
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 20:02:52 +0300

    Fix path to link home

--- src/displays/Home.js
+++ src/displays/Home.js
@@ -5,9 +5,36 @@ const Home = () => {
   return (
     <div className="home-section">
       <h2 className="home-title">Welcome to my page!</h2>
-      <p className="home-info">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi aspernatur odit eaque itaque, magni repellendus amet perferendis! Distinctio quae eos, molestiae laborum id sunt! Optio nostrum reprehenderit, illo nihil ea id a est aspernatur odio dolores voluptate qui animi nesciunt deleniti nobis harum maxime rerum! Enim earum assumenda recusandae quas voluptates facere porro officia. Nulla maxime maiores, ducimus, ab laboriosam atque veritatis fugiat debitis accusamus distinctio id sed quia ipsa impedit non quod aperiam voluptate perferendis velit mollitia repellat doloribus.
-      </p>
+      <div className="home-info">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+          obcaecati quae recusandae magni. Laboriosam error temporibus maiores
+          perferendis facere nulla sit perspiciatis quia deleniti impedit itaque
+          ex asperiores inventore labore hic at necessitatibus est dolorum, iure
+          ab corporis architecto quae?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+          quo alias tempora assumenda esse facilis. Reprehenderit magni rem
+          asperiores est laudantium ullam architecto reiciendis ratione aperiam
+          vero ducimus nobis voluptate, perspiciatis dolor, quaerat autem
+          temporibus.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+          quo alias tempora assumenda esse facilis. Reprehenderit magni rem
+          asperiores est laudantium ullam architecto reiciendis ratione aperiam
+          vero ducimus nobis voluptate, perspiciatis dolor, quaerat autem
+          temporibus.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati
+          quo alias tempora assumenda esse facilis. Reprehenderit magni rem
+          asperiores est laudantium ullam architecto reiciendis ratione aperiam
+          vero ducimus nobis voluptate, perspiciatis dolor, quaerat autem
+          temporibus.
+        </p>
+      </div>
     </div>
   );
 }
