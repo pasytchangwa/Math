fcd02370d20269b7aa2d494126d9ff43909a9828
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 18:00:19 +0300

    Update App.js file

--- src/App.js
+++ src/App.js
@@ -5,40 +5,24 @@ import CalcApp from './components/Calculator';
 import './components/style.css';
 import './App.css';
 import Nav from './Nav';
-import Home from './pages/Home';
-import Calc from './pages/Calc';
-import Quote from './pages/Quote';
+import Home from './sub-component/Home';
+import CalcPage from './sub-component/Calc';
+import Quote from './sub-component/Quote';
 
 const App = () => {
 return (
   <Router>
-    <div>
-     <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/calc">Calc</Link>
-        </li>
-        <li>
-          <Link to="/quote">Quote</Link>
-        </li>
-      </ul>
-     </nav>
-
-      <Switch>
-        <Route path="/calc">
-          <About />
-        </Route>
-        <Route path="/quote">
-          <Topics />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route path="/home">
+        <Home />
+      </Route>
+      <Route path="/calculator">
+        <CalcPage />
+      </Route>
+      <Route path="/quote">
+        <Quote />
+      </Route>
+    </Switch>
   </Router>
 );
 }
