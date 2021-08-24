4209d9829091975c1c23df614b5616257cc4602d
Sylvestre <pasytchangwa@gmail.com>
2021-08-24 16:31:39 +0300

    install react-router and create app.js file with basic routing skeleton

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
 /* eslint-disable max-classes-per-file, react/prefer-stateless-function */
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import CalcApp from './components/Calculator';
 import './components/style.css';
+import './App.css';
+import Nav from './Nav';
+import Home from './pages/Home';
+import Calc from './pages/Calc';
+import Quote from './pages/Quote';
 
-const App = () => <CalcApp />;
+const App = () => {
+return (
+  <Router>
+    <div>
+     <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/calc">Calc</Link>
+        </li>
+        <li>
+          <Link to="/quote">Quote</Link>
+        </li>
+      </ul>
+     </nav>
+
+      <Switch>
+        <Route path="/calc">
+          <About />
+        </Route>
+        <Route path="/quote">
+          <Topics />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+);
+}
 
 export default App;
