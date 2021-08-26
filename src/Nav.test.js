a0448f9e70a8333b604a3c936b34b1a72bfc8585
Sylvestre <pasytchangwa@gmail.com>
2021-08-26 19:20:08 +0300

    Add Nav.test.js file and Jest snapshot for testing Nav component

---
+++ src/Nav.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+test('renders correctly', () => {
+  const tree = renderer.create(
+    <Router>
+      <Link to='/home'>Home</Link>
+    </Router>,
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
